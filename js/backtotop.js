08476c97278bc69c3e2192fc057bf1be44fc1c8d
Larry Wang <larrywong9898@gmail.com>
2022-05-20 21:21:46 -0700

    add mobile nav control

--- js/backtotop.js
+++ js/backtotop.js
@@ -22,6 +22,21 @@
         window.addEventListener('load', toggleBacktotop)
         onscroll(document, toggleBacktotop)
     }
+    const scrollto = (el) => {
+        let header = select('#header')
+        let offset = header.offsetHeight
+    
+        if (!header.classList.contains('header-scrolled')) {
+          offset -= 16
+        }
+    
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+          top: elementPos - offset,
+          behavior: 'smooth'
+        })
+      }
+    
     on('click', '.scrollto', function(e) {
         if (select(this.hash)) {
           e.preventDefault()
@@ -47,6 +62,7 @@
             this.nextElementSibling.classList.toggle('dropdown-active')
         }
     }, true)
+    
 
 
 })()
